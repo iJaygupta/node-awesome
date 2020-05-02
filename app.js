ff12b6546ecbf19d09462b881f9a47132e743c5b
Jay Gupta <jayguptazzz@gmail.com>
2020-05-02 09:26:36 +0530

    package updated

--- app.js
+++ app.js
@@ -1,30 +1,28 @@
 var express = require("express");
 const app = express();
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
 require("dotenv").config();
-// var indexRouter = require("./routes/index");
 var apiRouter = require("./routes/api");
 var apiResponse = require("./utils/apiResponse");
 var cors = require("cors");
 
 
-// DB connection
-
 //don't show the log when it is test
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 //To allow cross-origin requests
-
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.status(200).send({
+        "error": false,
+        "code": 200,
+        "msg": "Welcome to Node Awesome !!",
+    });
+});
+
 //Route Prefixes
-// app.use("/", indexRouter);
 app.use("/api/", apiRouter);
 
 // throw 404 if URL not found
